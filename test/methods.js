b43b30d67c85a942c84ff3b332edabf96faf9e54
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2017-09-01 14:10:03 -0700

    Finish up most of the unit tests

--- test/methods.js
+++ test/methods.js
@@ -1,6 +1,7 @@
 var sinon = require('sinon');
 var should = require('should');
 var httpUtils = require('../lib/utils/httpUtils.js');
+var constants = require('../lib/utils/constants.js');
 var _ = require('underscore');
 var smartsheet = require('../index.js').createClient({accessToken: "token"});
 
@@ -39,6 +40,7 @@ describe('Method Unit Tests', function () {
                 { name: 'removeTemplatesFromFavorites', stub: 'delete', options: { objectId: 123 }, expectedRequest: {url: "2.0/favorites/template/123" }},
                 { name: 'removeSightsFromFavorites', stub: 'delete', options: { objectId: 123 }, expectedRequest: {url: "2.0/favorites/sight/123" }},
                 { name: 'removeWorkspacesFromFavorites', stub: 'delete', options: { objectId: 123 }, expectedRequest: {url: "2.0/favorites/workspace/123" }},
+                { name: 'removeWorkspacesFromFavorites', stub: 'delete', options: { id: 123 }, expectedRequest: {url: "2.0/favorites/workspace/123" }},
             ]
         },
         {
@@ -107,6 +109,12 @@ describe('Method Unit Tests', function () {
         {
             name: 'sheets',
             methods: [
+                { name: 'sendSheetViaEmail', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/emails"}},
+                { name: 'getPublishStatus', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/publish"}},
+                { name: 'setPublishStatus', stub: 'put', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/publish"}},
+                { name: 'updateSheet', stub: 'put', options: {}, expectedRequest: {url: "2.0/sheets/"}},
+                { name: 'deleteSheet', stub: 'delete', options: {}, expectedRequest: {url: "2.0/sheets/"}},
+                { name: 'moveSheet', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/move"}},
                 // attachments
                 { name: 'listAttachments', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/attachments/"}},
                 { name: 'listAttachmentVersions', stub: 'get', options: {sheetId: 123, attachmentId: 234}, expectedRequest: {url: "2.0/sheets/123/attachments/234/versions"}},
@@ -142,6 +150,46 @@ describe('Method Unit Tests', function () {
                 { name: 'createDiscussion', stub: 'post', options: {sheetId:123}, expectedRequest: {url: "2.0/sheets/123/discussions/"}},
                 { name: 'addDiscussionComment', stub: 'post', options: {sheetId:123, discussionId: 234}, expectedRequest: {url: "2.0/sheets/123/discussions/234/comments"}},
                 { name: 'deleteDiscussion', stub: 'delete', options: {sheetId:123, discussionId: 234}, expectedRequest: {url: "2.0/sheets/123/discussions/234"}},
+                // get
+                { name: 'getSheet', stub: 'get', options: {}, expectedRequest: {url: "2.0/sheets/"}},
+                { name: 'listSheets', stub: 'get', options: {}, expectedRequest: {url: "2.0/sheets/"}},
+                { name: 'getSheetAsCSV', stub: 'get', options: {}, expectedRequest: {url: "2.0/sheets/", accept: constants.acceptHeaders.textCsv}},
+                { name: 'getSheetAsExcel', stub: 'get', options: {}, expectedRequest: {url: "2.0/sheets/", accept: constants.acceptHeaders.vndMsExcel}},
+                { name: 'getSheetAsPDF', stub: 'get', options: {}, expectedRequest: {url: "2.0/sheets/", accept: constants.acceptHeaders.applicationPdf}},
+                { name: 'getSheetVersion', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/version"}},
+                { name: 'listOrganizationSheets', stub: 'get', options: {}, expectedRequest: {url: "2.0/users/sheets"}},
+                // rows
+                { name: 'getRow', stub: 'get', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234"}},
+                { name: 'getRowAttachments', stub: 'get', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/attachments"}},
+                { name: 'getRowDiscussions', stub: 'get', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/discussions"}},
+                { name: 'getCellHistory', stub: 'get', options: {sheetId: 123, rowId: 234, columnId: 345}, expectedRequest: {url: "2.0/sheets/123/rows/234/columns/345/history"}},
+                { name: 'copyRowToAnotherSheet', stub: 'post', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/copy"}},
+                { name: 'moveRowToAnotherSheet', stub: 'post', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/move"}},
+                { name: 'addRow', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/rows/"}},
+                { name: 'addRows', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/rows/"}},
+                { name: 'addRowUrlAttachment', stub: 'post', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/attachments"}},
+                { name: 'addRowFileAttachment', stub: 'postFile', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/attachments"}},
+                { name: 'createRowDiscussion', stub: 'post', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/discussions"}},
+                { name: 'sendRow', stub: 'post', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234/emails"}},
+                { name: 'deleteRow', stub: 'delete', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows?ids=234"}},
+                { name: 'updateRow', stub: 'put', options: {sheetId: 123, rowId: 234}, expectedRequest: {url: "2.0/sheets/123/rows/234"}},
+                { name: 'addImageToCell', stub: 'postFile', options: {sheetId: 123, rowId: 234, columnId: 345}, expectedRequest: {url: "2.0/sheets/123/rows/234/columns/345/cellimages"}},
+                // send update requests
+                { name: 'createUpdateRequest', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/updaterequests/"}},
+                { name: 'deleteUpdateRequest', stub: 'delete', options: {sheetId: 123, updateRequestId: 234}, expectedRequest: {url: "2.0/sheets/123/updaterequests/234"}},
+                { name: 'getUpdateRequest', stub: 'get', options: {sheetId: 123, updateRequestId: 234}, expectedRequest: {url: "2.0/sheets/123/updaterequests/234"}},
+                { name: 'getAllUpdateRequests', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/updaterequests/"}},
+                { name: 'changeUpdateRequest', stub: 'put', options: {sheetId: 123, updateRequestId: 234}, expectedRequest: {url: "2.0/sheets/123/updaterequests/234"}},
+                // update requests
+                { name: 'deleteSentUpdateRequest', stub: 'delete', options: {sheetId: 123, sentUpdateRequestId: 234}, expectedRequest: {url: "2.0/sheets/123/sentupdaterequests/234"}},
+                { name: 'getSentUpdateRequest', stub: 'get', options: {sheetId: 123, sentUpdateRequestId: 234}, expectedRequest: {url: "2.0/sheets/123/sentupdaterequests/234"}},
+                { name: 'getAllSentUpdateRequests', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/sentupdaterequests/"}},
+                // shares
+                { name: 'getShare', stub: 'get', options: {sheetId: 123, shareId: 234}, expectedRequest: {url: "2.0/sheets/123/shares/234"}},
+                { name: 'listShares', stub: 'get', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/shares/"}},
+                { name: 'share', stub: 'post', options: {sheetId: 123}, expectedRequest: {url: "2.0/sheets/123/shares/"}},
+                { name: 'deleteShare', stub: 'delete', options: {sheetId: 123, shareId: 234}, expectedRequest: {url: "2.0/sheets/123/shares/234"}},
+                { name: 'updateShare', stub: 'put', options: {sheetId: 123, shareId: 234}, expectedRequest: {url: "2.0/sheets/123/shares/234"}},
             ]
         },
         {
@@ -238,11 +286,6 @@ describe('Method Unit Tests', function () {
                     it('calls utils once', function () {
                         smartsheet[testGroup.name][method.name](method.options);
                         stub.callCount.should.be.equal(1);
-                    });
-
-                    it('request is correct', function () {
-                        smartsheet[testGroup.name][method.name](method.options);
-                        stub.args[0][0].should.have.properties(method.expectedRequest);
                     });
 
                     it('multiple requests are correct', function () {
