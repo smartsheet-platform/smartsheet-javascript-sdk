a1643a0bb49a2475dc13fcb8bd583a3b7c113c74
Nathan Armstrong <kenogu.labz@gmail.com>
2017-09-08 16:11:01 -0700

    Adjust response handler output contract.

--- test/responseHandler.js
+++ test/responseHandler.js
@@ -1,17 +1,16 @@
 var should = require('should');
 var Promise = require('bluebird');
 var _ = require('underscore');
-var responseHandler = require('../lib/utils/responseHandler');
+
+var handleResponse = require('../lib/utils/responseHandler');
 
 describe('Utils Unit Tests', function() {
   describe('#responseHandler', function() {
     describe('#handleResponse', function() {
-      var handleResponse = responseHandler.handleResponse;
-
       var mockResponse = null;
       var mockBody = null;
 
-      beforeEach(function() {
+      beforeEach(() => {
         mockResponse = {
           statusCode: 200,
           headers: {
@@ -23,12 +22,12 @@ describe('Utils Unit Tests', function() {
         mockBodyError = '{"errorCode": 911, "message":"EMERGENCY"}';
       });
 
-      afterEach(function() {
+      afterEach(() => {
         mockResponse = null;
         mockBody = null;
       });
 
-      it('should return a rejected promise if status code is not 200', function() {
+      it('should return a rejected promise if status code is not 200', () => {
         mockResponse.statusCode = 500;
         handleResponse(mockResponse, mockBodyError)
           .catch(function(error) {
@@ -38,16 +37,16 @@ describe('Utils Unit Tests', function() {
           });
       });
 
-      it('should return parsed JSON body', function() {
+      it('should return parsed JSON body', () => {
         var parsed = JSON.parse(mockBody);
         var result = handleResponse(mockResponse, mockBody);
-        result.hello.should.equal(parsed.hello);
+        result.content.hello.should.equal(parsed.hello);
       });
 
-      it('should return the body if content type is not application/json',function(){
+      it('should return the body if content type is not application/json', () => {
         mockResponse.headers['content-type'] = 'application/xml';
         var result = handleResponse(mockResponse, mockBody);
-        result.should.equal(mockBody);
+        result.content.should.equal(mockBody);
       });
     });
   });
