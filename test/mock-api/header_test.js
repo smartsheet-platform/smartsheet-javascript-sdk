2d7c66f072481dfa05073f786eb4d68be5ec60f8
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2018-03-02 14:07:45 -0800

    Update mock api tests to use base url constructor arg

--- test/mock-api/header_test.js
+++ test/mock-api/header_test.js
@@ -6,8 +6,6 @@ var helpers = require("./helpers");
 describe("Mock API SDK Tests", function() {
   var client = helpers.setupClient();
 
-  beforeEach(helpers.setupMockApiTest);
-
   describe("#Header", function() {
     var scenarios = [
         {
